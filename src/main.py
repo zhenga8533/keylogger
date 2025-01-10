14fa2d4452df602d07d798112121006e5af08bee
Allen Zheng <zhenga8533@gmail.com>
2025-01-10 12:33:54 -0500

    removed logging functionality

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
-from dotenv import load_dotenv
 from keylogger import Keylogger
-from util.logger import Logger
-import logging
-import os
 
-if __name__ == "__main__":
-    load_dotenv()
-    LOG = os.getenv("LOG") == "True"
 
-    logger = Logger("main", "logs/main.log", LOG)
-    logger.log(logging.INFO, "Hello, world!")
-
+def main():
     keylogger = Keylogger()
     keylogger.create_gui()
+
+
+if __name__ == "__main__":
+    main()
